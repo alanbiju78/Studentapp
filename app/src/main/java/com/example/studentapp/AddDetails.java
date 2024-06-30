cdcaf78323871ba6032f015fd99a603e290e66a6
alanbiju78 <alanbiju78@gmail.com>
2024-07-01 03:04:56 +0530

    Studentapp

---
+++ app/src/main/java/com/example/studentapp/AddDetails.java
@@ -0,0 +1,80 @@
+package com.example.studentapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class AddDetails extends AppCompatActivity {
+    EditText fname, lname, cname, dob, coname, num, email, address;
+    AppCompatButton subbt, gobackbt;
+    String apiurl="https://courseapplogix.onrender.com/addstudents";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_add_details);
+        fname = findViewById(R.id.fname);
+        lname = findViewById(R.id.lname);
+        cname = findViewById(R.id.cname);
+        dob = findViewById(R.id.dob);
+        coname = findViewById(R.id.coname);
+        num = findViewById(R.id.num);
+        email = findViewById(R.id.email);
+        address = findViewById(R.id.address);
+        subbt = findViewById(R.id.subbt);
+        gobackbt = findViewById(R.id.gobackbt);
+        subbt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String first_name = fname.getText().toString();
+                String last_name = lname.getText().toString();
+                String college_name = cname.getText().toString();
+                String date_of_birth = dob.getText().toString();
+                String course_name = coname.getText().toString();
+                String mobile_number = num.getText().toString();
+                String email_address = email.getText().toString();
+                String address_details = address.getText().toString();
+
+                JSONObject student = new JSONObject();
+                try {
+                    student.put("firstname", first_name);
+                    student.put("lastname", last_name);
+                    student.put("college", college_name);
+                    student.put("dob", date_of_birth);
+                    student.put("course", course_name);
+                    student.put("mobile", mobile_number);
+                    student.put("email", email_address);
+                    student.put("address", address_details);
+
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                        Request.Method.POST, apiurl, student, new Response.Listener<JSONObject>() {
+
+
+            }
+        });
+        gobackbt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/example/studentapp/MainActivity.java
+++ app/src/main/java/com/example/studentapp/MainActivity.java
@@ -1,19 +1,32 @@
 package com.example.studentapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    AppCompatButton addbt, viewbt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        addbt = findViewById(R.id.addbt);
+        addbt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), AddDetails.class);
+                startActivity(intent);
+            }
+        });
+        viewbt = findViewById(R.id.viewbt);
     }
 }
